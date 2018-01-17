8be82464d0ba2a9b635bbef3d6393ca924e0950f
linhaitao <linhaitao@finogeeks.com>
2018-01-17 14:35:34 +0800

    get hostname insteadof ip

--- scaling.sh
+++ scaling.sh
@@ -8,7 +8,8 @@ sleep 5
 GIDDYUP=/opt/rancher/bin/giddyup
 
 function scaleup {
-	MYIP=$($GIDDYUP ip myip)
+    MYIP=$(hostname)
+    #MYIP=$($GIDDYUP ip myip)
 	for IP in $($GIDDYUP ip stringify --delimiter " "); do
 		IS_MASTER=$(mongo --host $IP --eval "printjson(db.isMaster())" | grep 'ismaster')
 		if echo $IS_MASTER | grep "true"; then
